d30e0e767f67e637de89d1337fc4fe48a396f9be
shigehiro.ikeda <shigehiro.ikeda@excite-software.co.jp>
2021-04-24 20:52:27 +0900

    Project generated.

---
+++ app/src/main/java/app/ikd9684/android/study/custom_url_scheme_study/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.ikd9684.android.study.custom_url_scheme_study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
