b1aa30f00db74361d0ffb0514d756b30225eece5
shigehiro.ikeda <shigehiro.ikeda@excite-software.co.jp>
2021-04-24 22:53:42 +0900

    パラメータの取り方まで理解

--- app/src/main/java/app/ikd9684/android/study/custom_url_scheme_study/MainActivity.kt
+++ app/src/main/java/app/ikd9684/android/study/custom_url_scheme_study/MainActivity.kt
@@ -1,7 +1,8 @@
 package app.ikd9684.android.study.custom_url_scheme_study
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import app.ikd9684.android.study.custom_url_scheme_study.databinding.ActivityMainBinding
 
@@ -11,5 +12,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (intent.action == Intent.ACTION_VIEW) {
+            intent.data?.let { uri ->
+                val string = uri.scheme + "\n" +
+                        uri.host + "\n" +
+                        uri.path + "\n" +
+                        "param_a=${uri.getQueryParameter("param_a")},\n" +
+                        "param_b=${uri.getQueryParameter("param_b")},\n"
+
+                binding.textView.text = string
+            }
+        }
     }
 }
